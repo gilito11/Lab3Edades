9e370c9f31138767460c9efae7be7cbc95ee2d6a
XiaoLong Ji <96059963+xji650@users.noreply.github.com>
2023-11-02 18:45:01 +0100

    Delete part2 directory

--- part2/CuaInterface.java
+++
@@ -1,9 +0,0 @@
-package part2;
-
-public interface CuaInterface<E> {
-    public boolean isEmpty();
-
-    public void insert (E x);
-
-    public E remove();
-}
--- part2/main.java
+++
@@ -1,7 +0,0 @@
-package part2;
-
-public class main {
-    public static void main (String[]args){
-        System.out.println("");
-    }
-}
